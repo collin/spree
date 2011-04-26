a97f32af70142d669ef203181c0ed2af4d558961
David North <davidnorth@railsdog.com>
2011-04-26 15:08:44 +0100

    Cucumber scenario for promotion with landing page rule

--- core/app/controllers/content_controller.rb
+++ core/app/controllers/content_controller.rb
@@ -6,7 +6,7 @@ class ContentController < Spree::BaseController
   caches_page :show, :index, :if => Proc.new { Spree::Config[:cache_static_content] }
 
   def show
-    render :template => params[:path]
+    render :action => params[:path]
   end
 
   def cvv
